f4e1dd17445ca8b2db5d3da04bc63b958f32cd6a
keyankarthi <keyankarthisdk@gmail.com>
2020-08-30 19:34:24 +0530

    Updates

---
+++ precache-manifest.a4314969be355739b06cb49b656946d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21e2c5644991585530505328c69ede6",
+    "url": "/%7Blanding-page%7D/index.html"
+  },
+  {
+    "revision": "c068f165e6ba78f08e37",
+    "url": "/%7Blanding-page%7D/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3641c867b92dd3b7d266",
+    "url": "/%7Blanding-page%7D/static/css/main.0be7e223.chunk.css"
+  },
+  {
+    "revision": "c068f165e6ba78f08e37",
+    "url": "/%7Blanding-page%7D/static/js/2.03608bdb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/%7Blanding-page%7D/static/js/2.03608bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3641c867b92dd3b7d266",
+    "url": "/%7Blanding-page%7D/static/js/main.46ec692b.chunk.js"
+  },
+  {
+    "revision": "6476bce01e738aff04f1",
+    "url": "/%7Blanding-page%7D/static/js/runtime-main.5c5224ca.js"
+  }
+]);
